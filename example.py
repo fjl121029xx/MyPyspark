854cbce71f7d36f4ac5d03fbc9ab391c37969e5c
e <sishengqikuo_xx@163.com>
2018-11-28 17:31:09 +0800

    2018-11-28   ...1

---
+++ example.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'wsc'
+
+import sys
+
+i = 2
+while (i < 100):
+    j = 2
+    while j <= (i / j):
+        if not (i % j):
+            break
+        j = j + 1
+    if (j > i / j):
+        print(i, "是素数")
+        i = i + 1
+
+print("Good bye!")
