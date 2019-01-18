819f9673c6d3a94c3a2b0675dd6f9ee969e56e04
e <sishengqikuo_xx@163.com>
2019-01-18 11:03:56 +0800

    2019-01-18 ...1

---
+++ pyspark/hello_pyspark.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+__author__ = 'wsc'
+
+import os
+import sys
+
+# Path
+os.environ['SPARK_HOME'] = "H:\workspaces\spark"
+
+# Apperd pyspark to Python Path
+sys.path.append("H:\workspaces\spark\python")
+sys.path.append("H:\workspaces\spark\python\lib\py4j-0.10.7-src.zip")
+
+try:
+    from pyspark import SparkContext
+    from pyspark import SparkConf
+
+    print("Successfully imported Spark Modules")
+except ImportError as e:
+    print("Can not import Spark Modules", e)
+sys.exit(1)
---
+++ pyspark/work_count.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+__author__ = 'wsc'
+
+import os
+import sys
+
+# Path
+os.environ['SPARK_HOME'] = "H:\workspaces\spark"
+
+# Apperd pyspark to Python Path
+sys.path.append("H:\workspaces\spark\python")
+sys.path.append("H:\workspaces\spark\python\lib\py4j-0.10.7-src.zip")
+
+try:
+    from pyspark import SparkContext
+    from pyspark import SparkConf
+
+    print("Successfully imported Spark Modules")
+
+    if __name__ == '__main__':
+
+        # if len(sys.argv) != 3:
+        #     print("Usaee:python input_name output_name")
+        #     exit(1)
+
+        inputFile = "hello_pyspark.py"
+        outputFile = "1.txt"
+
+        sc = SparkContext()
+
+        text_file = sc.textFile(inputFile)
+        counts = text_file.flatMap(lambda line: line.split(' ')).map(lambda word: (word, 1)).reduceByKey(
+            lambda a, b: a + b)
+        counts.saveAsTextFile(outputFile)
+
+except ImportError as e:
+    print("Can not import Spark Modules", e)
+
+##
