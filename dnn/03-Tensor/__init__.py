7d1e3b9d609173d16dd1f1c4cd835e6345f7f949
fjl121029xx <sishengqikuo_xx@163.com>
2019-06-21 09:34:07 +0800

    2019-06-21   ...0

--- dnn/03-Tensor/__init__.py
+++ dnn/03-Tensor/__init__.py
@@ -7,12 +7,17 @@ import numpy
 import tensorflow as tf
 
 a = tf.constant([1.0, 2.0], name="a")
-b = tf.constant([[2.0, 3.0],[2.0, 3.0]], name="b")
+b = tf.constant([[2.0, 3.0], [2.0, 3.0]], name="b")
 
 result = a * b
-print(a)
-print(b)
-print(tf.get_default_graph)
+
+zerp = tf.zeros(shape=[10, 10], dtype=tf.float32, name="a")
+
+# 创建随机张量
+rn = tf.random_normal(shape=[10, 10], mean=0.0, stddev=1.0, dtype=tf.float32, seed=10, name="rn")
 
 with tf.Session() as sess:
-    print(sess.run(result))
+    # print(sess.run(result))
+    # print(zerp)
+    # print(sess.run(zerp))
+    print(sess.run(rn))
