793b56deba9833c1d4e42a7bbee0ba092d3dc5cc
fjl121029xx <sishengqikuo_xx@163.com>
2019-08-29 14:56:45 +0800

    2019-08-29  ...5 redis & mysql

--- mredis/__init__.py
+++ mredis/__init__.py
@@ -2,9 +2,15 @@
 # -*- coding:utf-8 -*-
 __author__ = 'fjl'
 
-import pandas as pd
-import numpy as np
-import tensorflow as tf
-import matplotlib.pyplot as plt
+"""
+使用python来操作redis用法详解
+https://www.jianshu.com/p/2639549bedc8
+"""
+import redis
 
-
+r = redis.Redis(host='192.168.100.20', port=6379,
+                decode_responses=True)  # host是redis主机，需要redis服务端和客户端都启动 redis默认端口是6379
+r.set('name', 'junxi')  # key是"foo" value是"bar" 将键值对存入redis缓存
+print(r['name'])
+print(r.get('name'))  # 取出键name对应的值
+print(type(r.get('name')))
